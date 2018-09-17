74e027ed3af5dbcdf73342b2b2a48f85bd89cfd0
Nhoppasit <dev.theprogram@gmail.com>
2018-09-17 23:23:52 +0700

    First commit

---
+++ src/db/Simple fixed asset/Sp_Asset_SearchFid.sql
@@ -0,0 +1,85 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+alter PROCEDURE Sp_Asset_SearchByFid
+	-- Add the parameters for the stored procedure here
+	@FID int,
+	@RowCount int output,
+	@MessageResult nvarchar(200) OUTPUT
+AS
+BEGIN			
+	-- ===================================================================================
+	-- STATE-1: VALIDATION
+	-- ===================================================================================	
+	/*-------------------------------------------------
+	 * เช็ค NULL
+	 *-------------------------------------------------*/
+	-- User id
+	IF @FID is NULL
+	begin
+		print 'Invalid TID! TID cannot be null.';
+		-- THROW 50031, 'Invalid TID! TID can not be NULL.', 1;  
+		set @RowCount=0;
+		set @MessageResult = 'Invalid TID! TID cannot be null.';
+		return -2;
+	end
+
+	-- ===================================================================================
+	-- STATE-2: QUERY FROM DATABASE
+	-- ===================================================================================
+	PRINT 'TID = ' + + RTRIM(CAST(@FID AS nvarchar(50)))
+	print 'SELECT and join data from TB_ASSET, TB_ROOM and TB_ASSET_TYPE'
+
+	BEGIN TRY
+
+		SELECT T1.EPC,T1.TID,T1.FID,T1.ASSET_LABEL,T1.ASSET_DESCRIPTION,T1.ASSET_TYPE,T2.ROOM_CODE,T2.ROOM_DESCRIPTION
+		from TB_ASSET T1
+		LEFT JOIN TB_ROOM T2
+		ON T1.ROOM_ID=T2.ID
+		WHERE T1.FID=@FID;
+						
+		SET @RowCount = @@ROWCOUNT;
+			
+		print 'Done selection'
+		IF @RowCount>0 -- data found.
+		BEGIN
+			SET @MessageResult = 'Asset found.';
+			return 0
+		END
+		ELSE -- data not found!
+		BEGIN
+			SET @MessageResult = 'Asset not found!';
+			return -1
+		END
+
+	END TRY
+	BEGIN CATCH
+		print 'QUERY ERROR'
+		SET @RowCount = 0;
+		SET @MessageResult = ERROR_MESSAGE();
+		return -2
+	END CATCH
+
+	-- SAFETY
+	return -99
+
+-- END OF STORE PROCEDURE.
+END
+GO
